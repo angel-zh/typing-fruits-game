76ee8febeb62b7318864b38da2ca2d5da5ef1d72
Angel Zhou <angel.zhou1016@gmail.com>
2022-09-20 13:01:52 -0400

    updates progress

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 
+// buttons
+const playGameBtn = document.getElementById('play-game-btn')
+const instructionsBtn = document.getElementById('instructions-btn')
+const okayBtn = document.getElementById('okay-btn')
+const xBtn = document.getElementById('x-btn')
+const normalBtn = document.getElementById('normal-btn')
+const hardBtn = document.getElementById('hard-btn')
+const bonusStartBtn = document.getElementById('bonus-start-btn')
+const playAgainBtn = document.getElementById('play-again-btn')
+
+//sections and divs
+const gameContainer = document.getElementById('game-container')
+const landingPage = document.getElementById('landing-page')
+const diffLvlPage = document.getElementById('difficulty-lvl-div')
+const instructionsPage = document.getElementById('instructions-div')
+const gameOverPage = document.getElementById('game-over-page')
+const winPage = document.getElementById('win-page')
+
+// others
+const textSection = document.getElementById('text-section')
+const prompt = document.getElementById('prompt')
+const timer = document.getElementById('timer')
+const userInput = document.getElementById('user-input')
+const scoreText = document.querySelectorAll('.scoreText')
+
 const normalPrompts = [
     "apple",
     "bananas",
@@ -19,12 +44,12 @@ const hardPrompts = [
     "We got bananas from the store.",
     "I ate a slice of cantaloupe.",
     "These cherries are delicious.",
-    "These lemons are so sour.",
+    "These lemons are too sour.",
     "I would like a mango please.",
     "An orange makes a great snack.",
     "My puppy loves yummy papaya.",
     "We grow peach trees in a yard.",
-    "Cut the pineapple in half.",
+    "Chop the pineapple in half.",
     "My brother picked strawberries.",
     "Please hand me a slice of watermelon."
 ]
@@ -44,50 +69,33 @@ const fruitImages = {
     watermelon: "game-images/watermelon.png"
 }
 
-// buttons
-const playGameBtn = document.getElementById('play-game')
-const instructionsBtn = document.getElementById('instrucitons')
-const okayBtn = document.getElementById('instr-okay')
-const normalBtn = document.getElementById('normal')
-const hardBtn = document.getElementById('hard')
-const bonusStartBtn = document.getElementById('bonus-start')
-const playAgainBtn = document.getElementById('play-again')
 
-//sections and divs
-const diffLvlPage = document.getElementById('difficulty-lvl-div')
-const instructionsPage = document.getElementById('instructions-div')
-const gameOverPage = document.getElementById('game-over-page')
-const winPage = document.getElementById('win-page')
-
-// others
-const prompt = document.getElementById('prompt')
-const timer = document.getElementById('timer')
-const userInput = document.getElementById('user-input')
-
-
-let difficulty = null
+let time = [6, 20] // time[0] is for part I, time[1] is for bonus level
 let randomArray = []
 let currentIndex = 0
-let time = 6 
 let score = 0
 let isGameOver = false
+let isTimesUp = false
 
 // sets the timer conditions and displays on the UI
-// to be passed into setInterval() function
-const runTimer = () => {
-    if (time === 0) {
-        isGameOver = true
+const runTimer = index => {
+    if (time[index] === 0) {
+        isTimesUp = true
+        const message = document.createElement('p')
+        message.innerHTML = "Time's up!"
+        message.style.color = "tomato"
+        timer.appendChild(message)
+        
     } else {
-        time--
+        time[index]--
     }
-    timer.innerHTML = `<span id="time">${time}</span>s left`
+    timerText.innerHTML = time
 }
-
 
 // in order to randomize the prompts in an array, use what is called 'Fisher-Yates Shuffle' algorithm
 // selects element of random index to switch with current element
 const randomizePrompts = array => { 
-    // clone original array
+    // clone original array to be used as prompt list
     randomArray = array.slice() 
     for (let i = randomArray.length - 1; i > 0; i--) { 
         // generates random index from 0 to i
@@ -101,16 +109,11 @@ const randomizePrompts = array => {
 }
 
 
-// function to display prompts listed in randomArray
-const displayPrompts = () => {
-    prompt.innerText = randomArray[currentIndex]
-}
-
-// compare user input to prompt
+// display prompts and compare user input text to prompt
 // if there is a match, move onto next index in randomArray and increase score
 const compareInput = () => {
-    displayPrompts()
-    if (userInput === prompt.innerText) {
+    prompt.innerText = randomArray[currentIndex]
+    if (userInput.value === prompt.innerText) {
         currentIndex++
         score += 10
         scoreText.innerText = score
@@ -120,16 +123,19 @@ const compareInput = () => {
 }
 
 const displayResult = () => {
-    if (isGameOver) {
-
+    if (isGameOver && isTimesUp) {
+        scoreText.innerText = score
+        gameContainer.classList.add('hide')
+        gameOverPage.classList.remove('hide')
     }
 }
 
 const startGame = () => {
-    timer.innerHTML = `<span id="time">${time}</span>s left`
+    timerText.innerHTML = time
 }
 
 
+// event listeners for all buttons
 playGameBtn.addEventListener('click', () => {
     diffLvlPage.classList.remove('hide')
 })
@@ -140,4 +146,22 @@ instructionsBtn.addEventListener('click', () => {
 
 okayBtn.addEventListener('click', () => {
     instructionsPage.classList.add('hide')
+})
+
+normalBtn.addEventListener('click', () => {
+    diffLvlPage.classList.add('hide')
+    landingPage.classList.add('hide')
+    randomizePrompts(normalPrompts)
+    textSection.classList.remove('hide')
+})
+
+hardBtn.addEventListener('click', () => {
+    diffLvlPage.classList.add('hide')
+    landingPage.classList.add('hide')
+    randomizePrompts(hardPrompts)
+    textSection.classList.remove('hide')
+})
+
+xBtn.addEventListener('click', () => {
+    diffLvlPage.classList.add('hide')
 })
