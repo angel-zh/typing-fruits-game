f6cbc3e5ec0a23d95fd53c3d6c8e1d31739f0390
Angel Zhou <angel.zhou1016@gmail.com>
2022-09-21 18:03:31 -0400

    adds bonus level game play and images

--- app.js
+++ app.js
@@ -16,20 +16,22 @@ const difficultyPage = document.getElementById('difficulty-div')
 const instructionsPage = document.getElementById('instructions-div')
 const textSection = document.getElementById('text-section')
 const imagesContainer = document.getElementById('images-container')
+const bonusPage = document.getElementById('bonus-page')
 const gameOverPage = document.getElementById('game-over-page')
 const winPage = document.getElementById('win-page')
+// const cells = document.querySelectorAll('.cells')
 // const timerDiv = document.getElementById('timer-div')
 
 // others
 const timerText = document.getElementById('timer-text')
 const promptText = document.getElementById('prompt-text')
 const userInput = document.getElementById('user-input')
-const scoreText = document.querySelectorAll('.scoreText')
+const scoreText = document.getElementById('scoreText')
 
 // array of objects storing each fruit's normal prompt, hard prompt, and image url
 const prompts = [
     { normal: "apple", hard: "My sister has a red apple.", url: "game-images/apple.png" },
-    { normal: "bananas", hard: "We got bananas from the store.", url: "game-images/banana.png" },
+    { normal: "bananas", hard: "We got bananas from the store.", url: "game-images/bananas.png" },
     { normal: "cantaloupe", hard: "I ate a slice of cantaloupe.", url: "game-images/cantaloupe.png" },
     { normal: "cherries", hard: "These cherries are delicious.", url: "game-images/cherries.png" },
     { normal: "lemons", hard: "These lemons are too sour.", url: "game-images/lemons.png" },
@@ -43,41 +45,12 @@ const prompts = [
 ]
 
 let difficulty = null
-let isGameOver = null
-let isTimesUp = false
-let promptsToShow = null
-let time = 6
+let isGameOver = false
+let time
 let currentIndex = 0
 let score = 0
 let timerId
 
-// const normalPrompts = Object.keys(fruits)
-// const hardPrompts = normalPrompts.map(prompt => {
-//     return prompts[prompt].hard
-// })
-
-
-
-const displayResult = () => {
-    if (isGameOver && isTimesUp) {
-        scoreText.innerText = score
-        gameContainer.classList.add('hide')
-        gameOverPage.classList.remove('hide')
-    }
-}
-
-// sets the timer conditions and displays on the UI
-const runTimer = () => {
-    if (time === 0) {
-        isTimesUp = true
-        isGameOver = true
-        
-    } else {
-        time--
-    }
-    timerText.innerText = time
-    displayResult()
-}
 
 // in order to randomize the prompts in an array, use what is called 'Fisher-Yates Shuffle' algorithm
 // selects element of random index to switch with current element
@@ -93,30 +66,79 @@ const shufflePrompts = array => {
     return prompts
 }
 
+
+// displays prompts based on difficulty level 
 const displayPrompts = () => {
-    if (difficulty === "normal") {
-        promptText.innerText = prompts[currentIndex].normal
-    } else if (difficulty === "hard")
-        promptText.innerText = prompts[currentIndex].hard
+    if (currentIndex < prompts.length) {
+        if (difficulty === "normal") {
+            promptText.innerText = prompts[currentIndex].normal
+        } else if (difficulty === "hard")
+            promptText.innerText = prompts[currentIndex].hard
+    } else if (currentIndex >= 12) {
+        textSection.classList.add('hide')
+        bonusPage.classList.remove('hide')
+        clearInterval(timerId)
+    }
 }
 
+// creates an image corresponding to the current prompt
 const createImage = () => {
-    const div = document.createElement('div')
     const img = document.createElement('img')
-    div.id = prompts[currentIndex-1].normal
-    img.src = prompts[currentIndex-1].url
-    div.appendChild(img)
+    img.src = prompts[currentIndex].url
+    img.id = prompts[currentIndex].normal
     imagesContainer.appendChild(img)
 }
 
-// display prompts and compare user text input to prompt
-// if there is a match, move onto next index and increase score
+// displays game over or winning screen
+const checkGameOver = () => {
+    if (isGameOver) {
+        gameContainer.classList.add('hide')
+        gameOverPage.classList.remove('hide')
+        playAgainBtn.classList.remove('hide')
+    } else if (!isGameOver && score === 240) {
+        gameContainer.classList.add('hide')
+        winPage.classList.remove('hide')
+    }
+}
+
+// sets the timer conditions and displays on the UI
+const runTimer = () => {
+    if (time === 0) {
+        isGameOver = true
+    } else {
+        time--
+    }
+    timerText.innerText = time
+
+}
+
+
+// compare user text input with prompt
+//if there is a match, load picture on screen, move onto next index and increase score
 const compareTextInput = () => {
     if (userInput.value === promptText.innerText) {
         createImage()
-        currentIndex++
         score += 10
         scoreText.innerText = score
+        currentIndex++
+        displayPrompts()
+        userInput.value = ''
+        if (difficulty === "normal") {
+            time = 6
+        } else {
+            time = 11
+        }
+    }
+}
+
+const compareClickedImg = event => {
+    const clickedImg = event.target.getAttribute('id')
+    if (clickedImg === prompts[currentIndex].normal) {
+        document.getElementById(clickedImg).style.visibility = 'hidden'
+        score += 10
+        scoreText.innerText = score
+        currentIndex++
+        displayPrompts()
     } else {
         isGameOver = true
     }
@@ -124,27 +146,36 @@ const compareTextInput = () => {
 
 
 
-
-
-
-
-
-// function to initialize the game by hiding intro pages and showing actual game page
+// initialize the game by invoking game functions
 const initGame = () => {
     difficultyPage.classList.add('hide')
     landingPage.classList.add('hide')
     textSection.classList.remove('hide')
+    imagesContainer.classList.remove('hide')
     timerText.innerText = time
+    scoreText.innerText = score
     shufflePrompts(prompts)
-    // createPrompts()
     displayPrompts()
     userInput.addEventListener('input', compareTextInput)
     timerId = setInterval(runTimer, 1000)
-
+    setInterval(checkGameOver, 100)
     
 }
 
-
+const startBonusLvl = () => {
+    bonusPage.classList.add('hide')
+    userInput.remove()
+    textSection.classList.remove('hide')
+    currentIndex = 0
+    timerText.innerText = time
+    difficulty = "normal"
+    shufflePrompts(prompts)
+    displayPrompts()
+    imagesContainer.addEventListener('click', compareClickedImg)
+    timerId = setInterval(runTimer, 1000)
+    setInterval(checkGameOver, 100)
+    
+}
 
 // event listeners for all buttons
 playGameBtn.addEventListener('click', () => {
@@ -165,10 +196,18 @@ xBtn.addEventListener('click', () => {
 
 normalBtn.addEventListener('click', () => {
     difficulty = "normal"
+    time = 6
     initGame()
 })
 
 hardBtn.addEventListener('click', () => {
     difficulty = "hard"
+    time = 11
     initGame()
 })
+
+bonusStartBtn.addEventListener('click', () => {
+    time = 20
+    startBonusLvl()
+})
+
